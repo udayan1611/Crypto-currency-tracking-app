510319d29af2cb25f5b53683c0d798fbd86fed6c
udayan1611 <157144796+udayan1611@users.noreply.github.com>
2025-04-01 17:21:40 +0530

    Add files via upload

---
+++ Signup.jsx
@@ -0,0 +1,19 @@
+import { Box, Heading } from "@chakra-ui/react";
+import React from "react";
+
+const Signup = () => {
+  return (
+    <Box
+      minH="80vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      bg="blackAlpha.900"
+      color="white"
+    >
+      <Heading size="2xl">Coming Soon...</Heading>
+    </Box>
+  );
+};
+
+export default Signup;
