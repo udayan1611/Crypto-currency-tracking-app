8e92396b36ce1b747802e6b5a8f005e36053fe7a
udayan1611 <157144796+udayan1611@users.noreply.github.com>
2025-04-08 16:51:10 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,29 +1,3 @@
-// import React from 'react'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import Header from './components/Header'
-// import Home from './components/Home'
-// import Coins from './components/Coins'
-// import Exchanges from './components/Exchanges'
-// import CoinDetails from './components/CoinDetails'
-// import Footer from './components/Footer'
-
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Header />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/coins" element={<Coins />} />
-//         <Route path="/exchanges" element={<Exchanges />} />
-//         <Route path="/coin/:id" element={<CoinDetails />} />
-//       </Routes>
-//       <Footer />
-//     </BrowserRouter>
-//   )
-// }
-
-// export default App
 
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
